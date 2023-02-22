5f3fb0fe094918d240717323d829ed1782d4fc66
Dzemz-z <egneradam@gmail.com>
2023-02-22 17:23:39 +0100

    a

--- testing.py
+++ testing.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(12,GPIO.OUT)
 pi_pwm = GPIO.PWM(12, 1000)
-pi_pwm.start(0)
-for i in range(100):
+pi_pwm.start(1)
+for i in range(1000):
     print("LED on")
     pi_pwm.ChangeFrequency(1)
     time.sleep(1)
     print("LED off")
-    pi_pwm.ChangeFrequency(100)
+    pi_pwm.ChangeFrequency(1000)
     time.sleep(1)
+pi_pwm.stop()
+GPIO.cleanup()

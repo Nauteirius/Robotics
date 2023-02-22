b1a219a0934426c451021aa5ca5c3afc863b8357
Dzemz-z <egneradam@gmail.com>
2023-02-22 17:21:43 +0100

    a

--- testing.py
+++ testing.py
@@ -7,7 +7,7 @@ pi_pwm = GPIO.PWM(12, 1000)
 pi_pwm.start(0)
 for i in range(100):
     print("LED on")
-    pi_pwm.ChangeFrequency(0)
+    pi_pwm.ChangeFrequency(1)
     time.sleep(1)
     print("LED off")
     pi_pwm.ChangeFrequency(100)

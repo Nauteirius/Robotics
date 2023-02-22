5ac368a9fd236f2357597a2df897adefedcfeb31
Nauteirius <kamil.sordyl@student.uj.edu.pl>
2023-02-22 17:12:45 +0100

    Adding sound emiting

--- sound.py
+++ sound.py
@@ -1,3 +1,15 @@
 from time import sleep
 import os
 import time
+import RPi.GPIO
+
+
+GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
+GPIO.setup(16, GPIO.OUT)# set GPIO 16 as an output. You can use any GPIO port
+p = GPIO.PWM(16, 200)    # create an object p for PWM on port 16 at 50 Hertz
+p.start(70)             # start the PWM on 70 percent duty cycle
+for x in range(200, 2200):
+      p.ChangeFrequency(x)  # change the frequency to x Hz (
+      time.sleep(0.0001)
+p.stop()                # stop the PWM output
+GPIO.cleanup()          # when your program exits, tidy up after yourself
